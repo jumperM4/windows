bfc352aff61f5f7c44e6a6b8f6b369d398dd107d
jumperM4 <pma1994ars@yandex.ru>
2023-04-08 14:13:23 +0300

    tabs switching

---
+++ src/js/modules/tabs.js
@@ -0,0 +1,45 @@
+const tabs = (
+  headerSelector,
+  tabSelector,
+  contentItemSelector,
+  activeClass
+) => {
+  const header = document.querySelector(headerSelector);
+  const tabs = document.querySelectorAll(tabSelector);
+  const contentItems = document.querySelectorAll(contentItemSelector);
+
+  const hideTabContent = () => {
+    contentItems.forEach((contentItem) => {
+      contentItem.style.display = "none";
+    });
+
+    tabs.forEach((tabItem) => {
+      tabItem.classList.remove(activeClass);
+    });
+  };
+
+  const showTabContent = (i = 0) => {
+    contentItems[i].style.display = "block";
+    tabs[i].classList.add(activeClass);
+  };
+
+  hideTabContent();
+  showTabContent();
+
+  header.addEventListener("click", (e) => {
+    const { target } = e;
+    if (
+      target.classList.contains(tabSelector.replace(/\./, "")) ||
+      target.parentNode.classList.contains(tabSelector.replace(/\./, ""))
+    ) {
+      tabs.forEach((tabItem, index) => {
+        if (target === tabItem || target.parentNode === tabItem) {
+          hideTabContent();
+          showTabContent(index);
+        }
+      });
+    }
+  });
+};
+
+export default tabs;
